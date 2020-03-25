a5224b2c7c5c7c70d39f3647c7a7fef1617fbd27
dimsonnGH <dimsonn@mail.ru>
2020-03-26 01:42:04 +0400

    Update views.py

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -52,24 +52,6 @@ def add_pokemon(folium_map, lat, lon, name, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    '''with open("pokemon_entities/pokemons.json", encoding="utf-8") as database:
-        pokemons = json.load(database)['pokemons']
-
-    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'], pokemon_entity['lon'],
-                pokemon['title_ru'], pokemon['img_url'])
-
-    pokemons_on_page = []
-    for pokemon in pokemons:
-        pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            #'title_ru': pokemon['title_ru'],
-            'title_ru': 'title_ru',
-        })'''
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
 
@@ -94,24 +76,6 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    '''with open("pokemon_entities/pokemons.json", encoding="utf-8") as database:
-        pokemons = json.load(database)['pokemons']
-
-    for pokemon in pokemons:
-        if pokemon['pokemon_id'] == int(pokemon_id):
-            requested_pokemon = pokemon
-            break
-    else:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
-
-    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon_entity in requested_pokemon['entities']:
-        add_pokemon(
-            folium_map, pokemon_entity['lat'], pokemon_entity['lon'],
-            pokemon['title_ru'], pokemon['img_url'])
-
-    return render(request, "pokemon.html", context={'map': folium_map._repr_html_(),
-                                                    'pokemon': pokemon})'''
 
     try:
         pokemon = Pokemon.objects.get(id=pokemon_id)
